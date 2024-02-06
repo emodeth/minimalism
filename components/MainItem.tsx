4ffd3c9d3ef089aa6ddd54caaee25810f6789e16
emodeth <emodethistaken@gmail.com>
2024-02-06 19:48:38 +0300

    links are added

--- components/MainItem.tsx
+++ components/MainItem.tsx
@@ -1,5 +1,6 @@
 import { StaticImageData } from "next/image";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type Props = {
@@ -12,16 +13,18 @@ type Props = {
 function MainItem({ name, description, path, image }: Props) {
   return (
     <div className="w-full aspect-square border cursor-pointer border-[#222] hover:border-[#fff] transition-all duration-[250ms] relative py-[15px] px-5 flex items-end rounded-xl">
-      <Image
-        className="invert-[1] w-full h-full object-cover opacity-40 absolute inset-0"
-        src={image}
-        fill
-        alt={name}
-      />
-      <div>
-        <p className="text-[14px]">{name}</p>
-        <p className="text-[14px] text-[#A3A3A3]">{description}</p>
-      </div>
+      <Link href={path}>
+        <Image
+          className="invert-[1] w-full h-full object-cover opacity-40 absolute inset-0"
+          src={image}
+          fill
+          alt={name}
+        />
+        <div>
+          <p className="text-[14px]">{name}</p>
+          <p className="text-[14px] text-[#A3A3A3]">{description}</p>
+        </div>
+      </Link>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import logo from "@/public/logo.svg";
 import MenuButton from "./MenuButton";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <nav className="bg-black border border-[#222] rounded-lg md:max-w-[630px] py-[15px] px-5 lg:px-6 lg:max-w-[960px] mx-auto">
       <ul className="flex items-center justify-between text-[12px]">
         <li className="uppercase transition-all cursor-pointer hover:text-[#A3A3A3]">
-          Minimalism Life®
+          <Link href="/">Minimalism Life®</Link>
         </li>
         <li>
           <MenuButton />
